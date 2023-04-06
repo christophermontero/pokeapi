9e3253c2c2563e77f77f757cabcc30a40462b252
Christopher Ortiz <cgortizm21@gmail.com>
2023-04-06 17:29:22 -0500

    Create a constant with all http responses

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import httpResponses from '../../utils/constants/responses';
 
 const validate =
   (validator: any) => (req: Request, res: Response, next: NextFunction) => {
-    const error = validator(req.body);
+    const { error } = validator(req.body);
     if (error)
       return res.status(400).json({
-        code: 'BAD_REQUEST',
-        message: 'Bad request',
+        code: httpResponses.BAD_REQUEST.code,
+        message: httpResponses.BAD_REQUEST.message,
         detail: error
       });
     next();
---
+++ utils/constants/responses.ts
@@ -0,0 +1,9 @@
+const httpResponses = {
+  BAD_REQUEST: {
+    httpCode: 400,
+    code: 'BAD_REQUEST',
+    message: 'Bad request'
+  }
+};
+
+export default httpResponses;
