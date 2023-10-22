717e04e4ca0bb3dd8bac1e692b1317c2d7094a6f
Christopher Ortiz <cgortizm21@gmail.com>
2023-10-22 11:17:40 -0500

    refactor(api): Add environment to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       watch: false,
       time: true,
       env: {
-        NODE_ENV: process.env.NODE_ENV,
+        NODE_ENV: 'production',
         POKEMON_BASE_URL: process.env.POKEMON_BASE_URL,
         MONGODB_URL: process.env.MONGODB_URL,
         JWT_SECRET: process.env.JWT_SECRET
